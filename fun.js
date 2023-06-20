083d061141b89a3bf9af096f98f885644cf201d6
Tanmayi Manku <tanmayitanum@gmail.com>
2023-06-20 15:31:27 +0530

    Updated JavaScript Linters

--- fun.js
+++ fun.js
@@ -1,21 +1,14 @@
-let btn = document.getElementById('hamburger');
-let menuBg = document.getElementById('menudrop');
-let menu = document.getElementById('menu-outline');
-let closee = document.getElementById('closebtn');
-
-
-
-btn.addEventListener('click', function(){
-    menu.style.display ='block';
-    menuBg.style.display = 'block';
-    
-})
-
- function closedMenu(){
-    menu.style.display ='none';
-    menuBg.style.display = 'none';
-    
+const btn = document.getElementById('hamburger');
+const menuBg = document.getElementById('menudrop');
+const menu = document.getElementById('menu-outline');
+const closee = document.getElementById('closebtn');
+btn.addEventListener('click', function (){
+  menu.style.display = 'block';
+  menuBg.style.display = 'block';
+});
+function closeMenu() {
+  menu.style.display = 'none';
+  menuBg.style.display = 'none';
 }
-closee.addEventListener('click', closedMenu)
-
-menu.addEventListener('click', closedMenu)
+closee.addEventListener('click', closeMenu);
+menu.addEventListener('click', closeMenu);
